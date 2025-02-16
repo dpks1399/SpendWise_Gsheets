71220008cf44cd0092cebd3cd1f58824c7c0284d
Deepak Sharma <dpks1399@gmail.com>
2025-02-16 14:55:14 +0530

    adding login page

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('add_transaction.html')
+    return render_template('login.html')
 
 @app.route('/api/get_categories', methods=['GET'])
 def get_categories():
